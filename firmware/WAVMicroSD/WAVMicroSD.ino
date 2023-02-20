6f577157b23111c64dc88db89c327a8de802b1f6
Sabas <s@electroniccats.com>
2023-02-20 09:46:07 -0600

    Update WAV SD

--- firmware/WAVMicroSD/WAVMicroSD.ino
+++ firmware/WAVMicroSD/WAVMicroSD.ino
@@ -34,14 +34,18 @@
 #include "hardware/pll.h"
 #include "hardware/clocks.h"
 
-#define fileName  "chona.wav"
+const char *fname1 =  "chona.wav";
+const char *fname2 =  "1_1.wav";
+
+static bool TriggerAudio = false;
+uint8_t range_mode = 1;
 
 File f;
 
 // Audio PIN is to match some of the design guide shields. 
 #define AUDIO_PIN 28  // you can change this to whatever you like
 
-#define AMP_EN 16 //Enable Ampli
+#define AMP_EN 14 //Enable Ampli
 
 int leds[4] = {10, 25, 26, 8};
 
@@ -159,14 +163,15 @@ void pwm_interrupt_handler() {
     } else {
         // reset to start
         wav_position = 0;
+        TriggerAudio = false;
     }
 }
 
-void readContents() {
+bool readContents(const char *fname) {
     wav_hdr wavHeader;
     int headerSize = sizeof(wav_hdr);
     
-    f = SD.open(fileName, FILE_READ);
+    f = SD.open(fname, FILE_READ);
     if (f) {
       size_t bytesRead = f.read(&wavHeader, headerSize);
       Serial.print("Header Read "); Serial.print(bytesRead); Serial.println(" bytes.");
@@ -263,47 +268,48 @@ void readContents() {
           Serial.print("File size is: "); Serial.print(fileSize); Serial.println(" bytes.");
           
           f.close();
-      
-          gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
-      
-          int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
-      
-          // Setup PWM interrupt to fire when PWM cycle is complete
-          pwm_clear_irq(audio_pin_slice);
-          pwm_set_irq_enabled(audio_pin_slice, true);
-          // set the handle function above
-          irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
-          irq_set_enabled(PWM_IRQ_WRAP, true);
-      
-          // Setup PWM for audio output
-          pwm_config config = pwm_get_default_config();
-          /* Base clock 176,000,000 Hz divide by wrap 250 then the clock divider further divides
-           * to set the interrupt rate. 
-           * 
-           * 11 KHz is fine for speech. Phone lines generally sample at 8 KHz
-           * 
-           * 
-           * So clkdiv should be as follows for given sample rate
-           *  8.0f for 11 KHz
-           *  4.0f for 22 KHz
-           *  2.0f for 44 KHz etc
-           */
-           
-          pwm_config_set_clkdiv(&config, 8.0f); 
-          pwm_config_set_wrap(&config, 250); 
-          pwm_init(audio_pin_slice, &config, true);
-      
-          pwm_set_gpio_level(AUDIO_PIN, 0);
-              
         }
-        //delete [] buffer;
-        //buffer = nullptr;
+        //delete [] WAV_DATA;
       }
       
     }
     else {
       Serial.println("File not found");
+      return false;
     }  
+    return true;
+}
+
+void enableINT(){
+  gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+      int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+  
+      // Setup PWM interrupt to fire when PWM cycle is complete
+      pwm_clear_irq(audio_pin_slice);
+      pwm_set_irq_enabled(audio_pin_slice, true);
+      
+      // set the handle function above
+      irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+      irq_set_enabled(PWM_IRQ_WRAP, true);
+  
+      // Setup PWM for audio output
+      pwm_config config = pwm_get_default_config();
+      pwm_config_set_clkdiv(&config, 8.0f); 
+      pwm_config_set_wrap(&config, 250); 
+      pwm_init(audio_pin_slice, &config, true);
+  
+      pwm_set_gpio_level(AUDIO_PIN, 0);
+}
+
+void disableINT(){
+  
+      // Setup PWM interrupt to fire when PWM cycle is complete
+      //pwm_clear_irq(audio_pin_slice);
+      //pwm_set_irq_enabled(audio_pin_slice, true);
+      
+      // set the handle function above
+      //irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+      irq_set_enabled(PWM_IRQ_WRAP, false);
 }
 
 void setup() 
@@ -336,13 +342,32 @@ void setup()
   }
   
   Serial.println("Initialization done.");
-  readContents();
+  //readContents();
 }
 
 void loop() 
 {
-  // nothing happens after setup
-  delay(500); 
-  // put your main code here, to run repeatedly:
-  __wfi(); // Wait for Interrupt
+  if (!TriggerAudio && range_mode == 1) {
+    enableINT();
+    Serial.println("Play 1");
+    if(readContents(fname1))
+    {
+      range_mode = 0;
+      TriggerAudio = true;
+    }
+  }
+  if (!TriggerAudio && range_mode == 0) {
+    enableINT();
+    Serial.println("Play 0");
+    if(readContents(fname2)){
+      range_mode = 1;
+      TriggerAudio = true;
+      
+    }
+  }
+  if(wav_position == 0){
+    disableINT();
+    delete [] WAV_DATA;
+    Serial.println("Interrupt!!");
+  }
 }
