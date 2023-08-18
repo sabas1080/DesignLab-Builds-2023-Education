f682c763e4ee043983a30c60157c4904e0f9d673
Sabas <s@electroniccats.com>
2023-08-17 21:32:21 -0600

    print size file and converter wav to wav

--- convertermp3towav.py
+++ convertermp3towav.py
@@ -9,6 +9,7 @@
 """
 import os
 from pydub import AudioSegment
+from humanize import naturalsize
 
 path = "audios"
 os.chdir(path)
@@ -19,7 +20,11 @@ for file in audio_files:
     name, ext = os.path.splitext(file)
     print(name)
     
+    size = os.stat(file).st_size
+    print(size)
+    
     if ext == ".m4a":
+        print('Converter m4a to wav') 
         sound = AudioSegment.from_file(file, format="m4a",duration = 10)
         sound = sound + 10
         #Configure Mono Channel, Frame Rate 11025, 8 bits
@@ -29,8 +34,14 @@ for file in audio_files:
         
         # export
         file_handle = sound.export("{0}.wav".format(name), format='wav')
+        
+        size = os.stat("{0}.wav".format(name)).st_size
+        print(naturalsize(size))
+        if(size > 31000):
+          print("'ALERTA: Es un archivo demasiado grande:'{0}.wav".format(name) )   
     if ext == ".mp3":
-        sound = AudioSegment.from_file(file, format="mp3",duration = 5)
+        print('Converter mp3 to wav')
+        sound = AudioSegment.from_file(file, format="mp3",duration = 10)
         sound = sound + 10
         #Configure Mono Channel, Frame Rate 11025, 8 bits
         sound = sound.set_channels(1)
@@ -39,4 +50,26 @@ for file in audio_files:
 
         # export
         file_handle = sound.export("{0}.wav".format(name), format='wav')
+        
+        size = os.stat("{0}.wav".format(name)).st_size
+        print(naturalsize(size))
+        if(size > 31000):
+          print("'ALERTA: Es un archivo demasiado grande:'{0}.wav".format(name) )    
+    if ext == ".wav":
+        print('Converter wav to wav')
+        sound = AudioSegment.from_file(file, format="wav",duration = 10)
+        sound = sound + 10
+        #Configure Mono Channel, Frame Rate 11025, 8 bits
+        sound = sound.set_channels(1)
+        sound = sound.set_frame_rate(11025)
+        sound = sound.set_sample_width(1)
+
+        # export
+        file_handle = sound.export("{0}.wav".format(name), format='wav')
+        
+        size = os.stat("{0}.wav".format(name)).st_size
+        print(naturalsize(size))
+        if(size > 31000):
+          print("'ALERTA: Es un archivo demasiado grande:'{0}.wav".format(name) )     
+          
 
